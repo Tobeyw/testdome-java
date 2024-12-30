e6de85322ec144acb1c92e8d8fbf15fbe6c5e4d5
Tobeyw <tobey1024@126.com>
2024-12-30 16:52:31 +0800

    Add comments

--- src/ParkingAllocation.java
+++ src/ParkingAllocation.java
@@ -1,8 +1,15 @@
 public class ParkingAllocation {
+    /*
+     * There are four situations where cars are given priority to park in large parking spaces if there are remaining large parking spaces
+     * 1. Large parking spaces are not enough to park buses
+     * 2. Large parking spaces can accommodate all buses and cars
+     * 3. Large parking spaces can accommodate all buses, and the remaining space can accommodate some cars, and small parking spaces can accommodate the remaining cars
+     * 4. Large parking spaces can accommodate all buses, and there are not enough parking spaces for cars
+     */
 
     public static int getMinimumSlots(int big, int small,  int buses,int cars) {
        
-        //First case:largeSlots Insufficient
+        //case 1:largeSlots Insufficient
         if (big<buses){
             return -1;
         }
@@ -17,15 +24,15 @@ public class ParkingAllocation {
         }else {
             bigslot_car_count = cars/3 +1;    
         }
-        // Second case：BigSlots has enough space for all buses and cars
+        //case 2：BigSlots has enough space for all buses and cars
         if (remainingBigSlots >= bigslot_car_count){  
             return  buses+ bigslot_car_count;
         }else{
-           //The third case: BigSlots has enough space for buses and and smallSlots has enough space cars(Cars park in bigslots first)
+           //case 3: BigSlots has enough space for buses and and smallSlots has enough space cars(Cars park in bigslots first)
            if(smallCarsInBigSlots + small >= cars){
              return big+cars-smallCarsInBigSlots;
            }else{
-            //The fourth case ：BigSlots are sufficient, smallSlots are insufficient
+            //case 4：BigSlots are sufficient, smallSlots are insufficient
             return -1;
            }
 
